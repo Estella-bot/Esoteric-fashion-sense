be930fdbf83e549f8c0b4be8e09e0f46c3083911
林晓洁 <linxiaojie@192.168.1.223>
2026-02-18 17:17:57 +0800

    Fix: remove extra parenthesis

--- app.py
+++ app.py
@@ -135,4 +135,4 @@ if __name__ == '__main__':
     # 在生产环境中，请使用更健壮的服务器，如gunicorn
     import os
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port))
+    app.run(host='0.0.0.0', port=port)
